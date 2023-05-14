445ef397a19a7dcc309bf441c59071cdd1eda0d5
boda <bodayasser687@gmail.com>
2023-05-14 22:15:14 +0300

    dashboard-fix-3

--- src/dashboard/cards/HomeCard.jsx
+++ src/dashboard/cards/HomeCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Homecard.scss"
+import "./HomeCard.scss";
 const HomeCard = ({ icon,iconbg,shadowbg, title, nums, percent, percentdesc }) => {
   return (
     <div className="homecard  bg-gray-50 ">
